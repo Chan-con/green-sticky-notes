54098e8832ada8c018543dfc0ba059e56834b88c
Chan-con <goa.kondo@gmail.com>
2025-08-25 03:21:42 +0900

    feat: 本番ビルドのパフォーマンス最適化
    - terser-webpack-pluginを追加してコード圧縮
    - 本番環境でconsole.logを自動削除
    - 本番環境でソースマップを無効化
    - ハードウェアアクセラレーションを無効化

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,29 +1,47 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/main/main.ts',
-    preload: './src/main/preload.ts'
-  },
-  target: 'electron-main',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: {
+      main: './src/main/main.ts',
+      preload: './src/main/preload.ts'
+    },
+    target: 'electron-main',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: isProduction,
+            },
+          },
+        }),
+      ],
+    },
+    devtool: isProduction ? false : 'inline-source-map',
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+  };
 };
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,35 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './src/renderer/index.tsx',
-  target: 'electron-renderer',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/renderer/index.tsx',
+    target: 'electron-renderer',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'renderer.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/renderer/index.html',
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'renderer.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/renderer/index.html',
-    }),
-  ],
-  devServer: {
-    port: 3000,
-  },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: isProduction,
+            },
+          },
+        }),
+      ],
+    },
+    devtool: isProduction ? false : 'inline-source-map',
+  };
 };
